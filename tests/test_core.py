8d6c8ec7a95516cee0ef2020aa935a4720a288fe
pared2021 <www1125446230@126.com>
2025-01-14 01:38:13 +0800

    refactor: remove old files from root directory

--- tests/test_core.py
+++
@@ -1,8 +0,0 @@
-import pytest
-from game_automation_suite.core import GameAutomationSuite
-
-def test_game_automation_suite():
-    suite = GameAutomationSuite()
-    assert isinstance(suite.game_control, GameAutomationSuite.game_control.__class__)
-    assert isinstance(suite.image_recognition, GameAutomationSuite.image_recognition.__class__)
-    assert isinstance(suite.user_interface, GameAutomationSuite.user_interface.__class__)
